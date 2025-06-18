46bbb74e46326e6db0af71bbc2362fb60f9e4794
Stephen Kim <green@hey.com>
2025-06-17 22:57:06 -0400

    Added connection to postgres db and middleware to log site visits

---
+++ sql/create-tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE request_logs (
+    id SERIAL PRIMARY KEY,
+    method TEXT,
+    path TEXT,
+    ip_address TEXT,
+    user_agent TEXT,
+    platform TEXT,
+    duration_ms INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
